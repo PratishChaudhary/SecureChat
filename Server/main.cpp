7b5038534ff32d5bcb176568f9ce2f3aaa5d9783
Aayushhh <aayushbist50@gmail.com>
2026-07-10 12:04:45 +0545

    Update main.cpp
    added comments

--- Server/main.cpp
+++ Server/main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     // Create the main application window.
      MainWindow w;
+     // Display the main window.
     w.show();
     return a.exec();
 }
